960a52b2e0507763f74750cdf0e52ccff7f42d4e
kadakaskevin <kevin.kadakas@digiteenused.ee>
2025-08-26 15:15:54 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("hello-btn");
+  if (btn) {
+    btn.addEventListener("click", function () {
+      const newTab = window.open("", "_blank");
+      newTab.document.write("<h1>Hello</h1>");
+      newTab.document.close();
+    });
+  }
+});
